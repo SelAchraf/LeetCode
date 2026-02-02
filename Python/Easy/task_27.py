4061843912ba4236c88c36b89d82b7447d012d35
SelAchraf <seloughamedachraf@gmail.com>
2026-02-02 11:04:25 +0100

    Implement solution for removing elements from an array

---
+++ Python/Easy/task_27.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        
+        if not nums:
+            return 0
+        
+        k = 0
+        
+        for i in range (len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        print(nums[:k])
+        print(k)
+        return k
+
+solution = Solution()
+nums = [3,2,2,3]
+result = solution.removeElement(nums, 3)
